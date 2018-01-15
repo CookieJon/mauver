1dbb24d60dc6080d6caa0a44485e3478ee6a8556
Jon <cookie.jon@gmail.com>
2018-01-15 18:02:40 +1100

    ORM OX to NEWT
    ORM OX to NEWT

---
+++ src/store/models/Todo.js
@@ -0,0 +1,19 @@
+import { Model } from 'vuex-orm'
+import User from './User'
+
+export default class Todo extends Model {
+  static entity = 'todos'
+
+  static fields () {
+    return {
+      id: this.attr(null),
+      user_id: this.attr(null),
+      title: this.attr(''),
+      done: this.attr(false),
+      assignee: this.belongsTo(User, 'user_id'),
+      flip () {
+        alert('glip')
+      }
+    }
+  }
+}
